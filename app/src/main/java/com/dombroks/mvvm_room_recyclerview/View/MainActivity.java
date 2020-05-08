cdd7213d817b3a7010ed4f7eec982aadfab38c9b
dombroks <tinoutech@gmail.com>
2020-05-08 21:39:06 +0100

    initial commit

--- app/src/main/java/com/dombroks/mvvm_room_recyclerview/View/MainActivity.java
+++ app/src/main/java/com/dombroks/mvvm_room_recyclerview/View/MainActivity.java
@@ -1,8 +1,10 @@
-package com.dombroks.mvvm_room_recyclerview;
+package com.dombroks.mvvm_room_recyclerview.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.dombroks.mvvm_room_recyclerview.R;
 
 public class MainActivity extends AppCompatActivity {
 
