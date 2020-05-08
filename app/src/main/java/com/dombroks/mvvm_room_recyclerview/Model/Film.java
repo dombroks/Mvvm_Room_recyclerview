de92f564cccd67e1364c39fc0d6fb02c23395530
dombroks <tinoutech@gmail.com>
2020-05-08 16:59:33 +0100

    initial commit

---
+++ app/src/main/java/com/dombroks/mvvm_room_recyclerview/Model/Film.java
@@ -0,0 +1,28 @@
+package com.dombroks.mvvm_room_recyclerview.Model;
+
+public class Film {
+    private int id ;
+    private String title;
+    private String description;
+
+    public Film(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
