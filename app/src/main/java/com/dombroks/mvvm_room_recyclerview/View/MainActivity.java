2b679c754494c2ec6178877b610462b96263771c
dombroks <tinoutech@gmail.com>
2020-05-08 22:17:17 +0100

    initial commit

--- app/src/main/java/com/dombroks/mvvm_room_recyclerview/View/MainActivity.java
+++ app/src/main/java/com/dombroks/mvvm_room_recyclerview/View/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.dombroks.mvvm_room_recyclerview.R;
+import com.dombroks.mvvm_room_recyclerview.ViewModel.FilmViewModel;
 
 public class MainActivity extends AppCompatActivity {
+    private FilmViewModel filmViewModel;
+    public static final int ADD_REQUEST=1 ;
+    public static final int EDIT_REQUEST=2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
