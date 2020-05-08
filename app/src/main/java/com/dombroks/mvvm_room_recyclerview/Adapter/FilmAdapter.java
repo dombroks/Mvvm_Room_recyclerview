a6751208feb35796eb663fe8a94fb2611a73f11a
dombroks <tinoutech@gmail.com>
2020-05-08 23:25:52 +0100

    initial commit

---
+++ app/src/main/java/com/dombroks/mvvm_room_recyclerview/Adapter/FilmAdapter.java
@@ -0,0 +1,49 @@
+package com.dombroks.mvvm_room_recyclerview.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dombroks.mvvm_room_recyclerview.Model.Film;
+import com.dombroks.mvvm_room_recyclerview.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilmAdapter extends RecyclerView.Adapter<FilmAdapter.Holder> {
+    private List<Film> films = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item, parent, false);
+        return new Holder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        Film current=films.get(position);
+        holder.tv_title.setText(current.getTitle());
+        holder.tv_description.setText(current.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return films.size() ;
+    }
+
+    class Holder extends RecyclerView.ViewHolder {
+        private TextView tv_title;
+        private TextView tv_description;
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+            tv_title=itemView.findViewById(R.id.tv_title) ;
+            tv_description=itemView.findViewById(R.id.tv_description);
+        }
+    }
+}
--- app/src/main/java/com/dombroks/mvvm_room_recyclerview/View/MainActivity.java
+++ app/src/main/java/com/dombroks/mvvm_room_recyclerview/View/MainActivity.java
@@ -1,20 +1,42 @@
 package com.dombroks.mvvm_room_recyclerview.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.dombroks.mvvm_room_recyclerview.Model.Film;
 import com.dombroks.mvvm_room_recyclerview.R;
 import com.dombroks.mvvm_room_recyclerview.ViewModel.FilmViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private FilmViewModel filmViewModel;
     public static final int ADD_REQUEST=1 ;
     public static final int EDIT_REQUEST=2;
+    private FloatingActionButton floatingActionButton ;
+    private TextView title;
+    private TextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        filmViewModel=new ViewModelProvider(this).get(FilmViewModel.class);
+        filmViewModel.getAllNotes().observe(new Observer<List<Film>>() {
+            @Override
+            public void onChanged(List<Film> films) {
+                
+            }
+        });
     }
 }
